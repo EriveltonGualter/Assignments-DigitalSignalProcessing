9d495b625ce5f30a0ab56648aa9d286526b3d7f5
Erivelton Gualter dos Santos <erivelton.gualter@gmail.com>
2016-04-18 12:45:19 -0300

    add Assignment 7

--- Aula 6/Aula06_ex01.m
+++ Aula 6/Aula06_ex01.m
@@ -1,38 +1,38 @@
 % Aula 6 - Processamento de Imagens %
-clear all
+clear all, close all
 
 load('Aula06_ex01.mat');
 
-% % Numero de pixels da imagem e numero de cores
-% n_pixels = size(X,1)*size(X,2);
-% n_cores = length(map);
+% Numero de pixels da imagem e numero de cores
+n_pixels = size(X,1)*size(X,2);
+n_cores = length(map);
+
+% Carregando a image ( cores não-naturais)
+figure(1);
+image(X);
+
+% Usando as cores do map
+figure(2);
+colormap(map);
+image(X);
 % 
-% % Carregando a image ( cores não-naturais)
-% figure(1);
-% image(X);
-% 
-% % Usando as cores do map
-% figure(2);
-% colormap(map);
-% image(X);
-% 
-% % Alteração do brilho
-% figure(3);
-% colormap(map);
-% image(X);
-% brighten(0.5);
-% 
-% % Ampliação do Olho
-% figure(4);
-% Xolho = X(20:140, 150:225);
-% image(Xolho);
-% colormap(map);
-% 
-% % Ampliação do Nariz
-% figure(5);
-% Xolho = X(90:160, 20:120);
-% image(Xolho);
-% colormap(map);
+% Alteração do brilho
+figure(3);
+colormap(map);
+image(X);
+brighten(0.5);
+
+% Ampliação do Olho
+figure(4);
+Xolho = X(20:140, 150:225);
+image(Xolho);
+colormap(map);
+
+% Ampliação do Nariz
+figure(5);
+Xolho = X(90:160, 20:120);
+image(Xolho);
+colormap(map);
 
 figure(5);
 
